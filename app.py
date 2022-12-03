2dddfd3fe96fa4fd393cfe90c05220846bef9e0c
Colette Ulianogit config --global --listclear <culiano@live.com>
2022-12-02 21:30:01 -0500

    update app

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ SPECS
 Handle the actions of a game character
 
 character traits:
-    Name
-    Power Points
-    Can teleport
+    // Name
+    // Power Points
+    // Can teleport
 
 needs to perform actions:
 
@@ -29,8 +29,8 @@ needs to perform actions:
     invalid actions
         //- print 'Invalid Action'
 
-considerations:
-    player can only have 0 to 10 power points.
+! considerations:
+    ! player can only have 0 to 10 power points.
 """
 
 """
@@ -40,6 +40,33 @@ SPECS
     isolate the action functionality into a function
     randomly select action
 """
+
+# just moved everything into this function
+def characterAction(action, character):
+    name, power_points, can_teleport = character.values()
+    if action == 'run':
+        if power_points > 0:
+            power_points -= 1
+            print ( f'{name} is running' )
+        else:
+            print ( 'not enough power points to run')
+    elif action == 'rest':
+        if power_points < 10:
+            print( 'resting' )
+            power_points += 1
+        else:
+            print( 'no rest needed' )
+    elif action == "teleport":
+        if can_teleport:
+            if power_points >= 2:
+                power_points -= 2
+                print(f"{name} is teleporting")
+            else:
+                print("not enough powerpoints")
+        else:
+            print('unable to teleport')
+    else:
+        print ( "Invalid Command" )
 
 # ? all the characters into a list (array), each element of the list a dictionary (object) for each character
 characters = [
@@ -55,40 +82,34 @@ characters = [
     }
 ]
 
-# ? deconstruct the dictionary (object) so each key is a variable with it's value
-name, power_points, can_teleport = characters.values()
+
+# ? randomly select an action
+import random
 
 # ? iterate through. So for each character in the characters dictionary it will print all their details
 for character in characters:
+# ? deconstruct the dictionary (object) so each key is a variable with it's value
+    name, power_points, can_teleport = character.values()
+
     print ( f'{name} has {power_points} Power Points and {"can" if can_teleport else "cannot"} teleport' )
 
-action = 'teleport' # run, rest, teleport 
+    # ? action functionality into a function
+    # update this to a list of all actions
+    actions = [
+        'teleport',
+        'rest',
+        'run'
+    ]
+    # ? pick a random action
+    action = random.choice(actions)
 
-if action == 'run':
-    if power_points > 0:
-        power_points -= 1
-        print ( f'{name} is running' )
-    else:
-        print ( 'not enough power points to run')
-elif action == 'rest':
-    if power_points < 10:
-        print( 'resting' )
-        power_points += 1
-    else:
-        print( 'no rest needed' )
-elif action == "teleport":
-    if can_teleport:
-        if power_points >= 2:
-            power_points -= 2
-            print(f"{name} is teleporting")
-        else:
-            print("not enough powerpoints")
-    else:
-        print('unable to teleport')
-else:
-    print ( "Invalid Command" )
+    characterAction(action, character)
 
-print ( power_points )
+
+
+
+
+
 
 """
 SPECS
@@ -96,12 +117,12 @@ SPECS
     // Add an 'inventory' action that displays all the items in inventory
 """
 
-items = [
-    'sword',
-    'shield',
-    'potion'
-]
+# items = [
+#     'sword',
+#     'shield',
+#     'potion'
+# ]
 
-for i in range(len(items)):
-    print(items[i])
+# for i in range(len(items)):
+#     print(items[i])
 
